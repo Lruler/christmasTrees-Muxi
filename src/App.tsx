c899d5fe03a6bcca542f6dbc951d27a8a28a7b5b
lruler <1204312199@qq.com>
2021-12-20 16:32:46 +0800

    add download

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import img from './avatar.png'
+import comment from './comment.svg'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const location = window.location.href.slice(0, -1) + img
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <img src={img} alt="img" />
+      <a href={img} download>点击下载</a>
+      <img src={comment} alt="comment" />
+      <a href={comment} download>点击下载图片二</a>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
