78dcb4f0dbd411c7cbcd2f262cf8ab90b7d002e9
lruler <1204312199@qq.com>
2021-12-23 09:35:35 +0800

    完成end页面canvas

--- src/pages/End/index.jsx
+++ src/pages/End/index.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router";
+import { Stage, Layer, Image } from "react-konva";
+import useImage from "use-image";
 import Message from "../../components/Message";
 import Header from "../../components/Header";
+import xuehua from "../../static/other/xuehua.svg";
+import rahmen from "../../static/other/rahmen.svg";
 import "./index.css";
 
-const width = window.innerWidth;
+const DownLoadImage = (props) => {
+  const { x, y } = props;
+  const [image] = useImage(rahmen);
+  const rahmenX = x
+  return <Image image={image} />;
+};
+
 const End = () => {
   const location = useLocation();
   const { state } = location;
   const [isTip, setIsTip] = useState(false);
-
+  const width = window.innerWidth;
+  const height = window.innerHeight * 0.9;
   const handleDownload = () => {
     if (width < 1000) {
       setIsTip(true);
@@ -28,8 +39,15 @@ const End = () => {
   return (
     <div className="end-container">
       <Header />
-      <img src={state} alt="" />
-      <button onClick={handleDownload}>下载图片</button>
+      <img src={xuehua} className="xuehua"></img>
+      <Stage width={width} height={height}>
+        <Layer>
+          <DownLoadImage x={width} y={height} />
+        </Layer>
+      </Stage>
+      <div onClick={handleDownload} className="downloadtree">
+        下载图片
+      </div>
       {isTip && <Message />}
     </div>
   );
